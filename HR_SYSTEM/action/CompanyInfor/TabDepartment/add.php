f7ca157a70c6d37ac1c0eb796c9544ddabe2d140
Hongly21 <hongly06082004@gmail.com>
2025-08-14 17:49:48 +0700

    add
    finish hrcompany

---
+++ HR_SYSTEM/action/CompanyInfor/TabDepartment/add.php
@@ -0,0 +1,24 @@
+<?php
+   include('../../../Config/conect.php');
+
+
+   if(isset($_POST['action'])){
+    if($_POST['action'] == 'btnsave'){
+        $comCode = $_POST['comCode'];
+        $comName = $_POST['comName'];
+        $comStatus = $_POST['comStatus'];
+
+
+        $sql="INSERT INTO `hrdepartment` (`Code`, `Description`, `Status`) VALUES ( '$comCode', '$comName', '$comStatus');";
+        $result = $con->query($sql);
+        if($result){
+            echo "Success";
+   
+        }else{
+            echo "Can not add";
+        }
+    }
+   }
+
+
+?>
